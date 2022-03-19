87ca366332728bbaca0786972017a915a391c1fa
prathamesh25ig <prathameshbachhav25@gmail.com>
2022-03-19 14:20:40 +0530

    MODIFIED: Calculating length of Line 1

--- src/com/infogalaxy/LineComparisonProblem.java
+++ src/com/infogalaxy/LineComparisonProblem.java
@@ -1,8 +1,25 @@
 package com.infogalaxy;
 
+import java.util.Scanner;
+
 public class LineComparisonProblem {
     public static void main (String args[]){
         //Display Welcome Line To User:
         System.out.println("Welcome To Line Comparison Problem Computation Program On Master Branch");
-    }
+        Scanner sc = new Scanner(System.in);
+        double x1;
+        double y1;
+        double x2;
+        double y2;
+        double length;
+        System.out.println("Enter the X1 co-ordinates");
+        x1 = sc.nextDouble();
+        System.out.println("Enter the X2 co-ordinates");
+        x2 = sc.nextDouble();
+        System.out.println("Enter the Y1 co-ordinates");
+        y1 = sc.nextDouble();
+        System.out.println("Enter the Y2 co-ordinates");
+        y2 = sc.nextDouble();
+
+}
 }
